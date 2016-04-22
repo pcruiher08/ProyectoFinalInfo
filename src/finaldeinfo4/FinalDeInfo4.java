a9eca9f0385fd37e8af384d67ba555d03a1c66c0
Pablo <pcruiher08@yahoo.com.mx>
2016-04-22 14:05:43 -0500

    Actualizacion

--- src/finaldeinfo4/FinalDeInfo4.java
+++ src/finaldeinfo4/FinalDeInfo4.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package finaldeinfo4;
 
 import java.util.ArrayList;
@@ -28,6 +24,8 @@ public class FinalDeInfo4 {
         Inventario.add("Cosa1");
         Inventario.add("Cosa2");
         Inventario.add("Cosa3");
+        Inventario.add("Cosa3.1");
+        Inventario.add("Cosa3.2");
         Inventario.add("Cosa4");
         Inventario.remove(Inventario.indexOf("Cosa2"));
         for(int i=0; i<Inventario.size(); i++)
