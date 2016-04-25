b3b60ce8dde11b7a96915da054409307a76b6644
Pablo <pcruiher08@yahoo.com.mx>
2016-04-25 15:43:13 -0500

    lista de dos dimentions- done

--- src/finaldeinfo4/FinalDeInfo4.java
+++ src/finaldeinfo4/FinalDeInfo4.java
@@ -13,34 +13,40 @@ public class FinalDeInfo4 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         List<List<String>> listOfList = new ArrayList<>();
         listOfList.add(new ArrayList<>());
-        listOfList.get(0).add(new String());
+        listOfList.get(0).add("Tomatoes");
+        listOfList.add(new ArrayList<>());
+        listOfList.get(1).add("Lechugas");
+        listOfList.get(1).add("1");
+        listOfList.get(1).add("2");
+        listOfList.get(1).remove(1);
         
         
-        
-        ArrayList<String> Inventario = new ArrayList<String>();
-        Inventario.add("Cosa1");
-        Inventario.add("Cosa2");
-        Inventario.add("Cosa3");
-        Inventario.add("Cosa4");
-        Inventario.add("Cosa5");
-        Inventario.add("Cosa6");
-        Inventario.remove(Inventario.indexOf("Cosa2"));
-        for(int i=0; i<Inventario.size(); i++)
-        System.out.println(Inventario.get(i));
-        
-        
+//        ArrayList<String> Inventario = new ArrayList<String>();
+//        Inventario.add("Cosa1");
+//        Inventario.add("Cosa2");
+//        Inventario.add("Cosa3");
+//        Inventario.add("Cosa4");
+//        Inventario.add("Cosa5");
+//        Inventario.add("Cosa6");
+//        Inventario.remove(Inventario.indexOf("Cosa2"));
+//        for(int i=0; i<Inventario.size(); i++)
+//        System.out.println(Inventario.get(i));
+//        Inventario.add(ListaNueva(Class<T>klazz));        
     }
+    public <T> List<T> ListaNueva(Class<T>klazz){
+   List<T> lista = new ArrayList<>();
+   return lista;
+   }
     
-    public void agregaUnaLista(){
-    Inventario.add(ListaNueva("o")); //checar el error en esta linea
-    }
+//    public void agregaUnaLista(){
+//    Inventario.add(ListaNueva()); //checar el error en esta linea
+//    }
     
-   public <T> List<T> ListaNueva(Class<T>klazz, String tomatos){
-   List<T> tomatos = new ArrayList<>();
-   return tomatos;
+  
+   
    
    /* Estructura de datos en C++
    
@@ -51,6 +57,6 @@ public class FinalDeInfo4 {
 
     */
    
-   }
+   
     
 }
