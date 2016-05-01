ca5b42d5f04e5e214b976140477319bc4b07303d
Pablo <pcruiher08@yahoo.com.mx>
2016-05-01 03:34:34 -0500

    casi terminado por checar bug de espacios en listas y falta boton de borrar todo tambien checar las restas que llevan a productos negativos y lo que se imprime en GUI

--- src/finaldeinfo4/FinalDeInfo4.java
+++ src/finaldeinfo4/FinalDeInfo4.java
@@ -1,45 +1,69 @@
 
 package finaldeinfo4;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
  * @author Pablo
  */
-public class FinalDeInfo4 {
+public class FinalDeInfo4{
+
+    /**
+     *
+     */
+    //public static List<List<String>> listOfList;
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)  {
-        List<List<String>> listOfList = new ArrayList<>();
-        listOfList.add(new ArrayList<>());
-        listOfList.get(0).add("Tomatoes");
-        listOfList.add(new ArrayList<>());
-        listOfList.get(1).add("Lechugas");
-        listOfList.get(1).add("1");
-        listOfList.get(1).add("2");
-        listOfList.get(1).remove(1);
-        
-        
-//        ArrayList<String> Inventario = new ArrayList<String>();
-//        Inventario.add("Cosa1");
-//        Inventario.add("Cosa2");
-//        Inventario.add("Cosa3");
-//        Inventario.add("Cosa4");
-//        Inventario.add("Cosa5");
-//        Inventario.add("Cosa6");
-//        Inventario.remove(Inventario.indexOf("Cosa2"));
-//        for(int i=0; i<Inventario.size(); i++)
-//        System.out.println(Inventario.get(i));
-//        Inventario.add(ListaNueva(Class<T>klazz));        
+    public static void main(String[] args){
+//        List<List<String>> listOfList = new ArrayList<>();
+//        listOfList.add(new ArrayList<>());
+//        listOfList.get(0).add("Tomatoes");
+//        listOfList.get(0).add("1");
+//        listOfList.get(0).add("2");
+//        listOfList.add(new ArrayList<>());
+//        listOfList.get(1).add("Lechugas");
+//        listOfList.get(1).add("1");
+//        listOfList.get(1).add("2");
+//        listOfList.add(new ArrayList<>());
+//        listOfList.get(2).add("Salchichas");
+//        listOfList.get(2).add("1");
+//        listOfList.get(2).add("2");
+//       // agregando(2,"Pablo");
+//        //listOfList.get(2).remove("1");
+//        //listOfList.get(1).remove(1);
+//        for(int i=0; i<listOfList.size(); i++){
+//        System.out.println(listOfList.get(i));
+//        }
+//        
+//       
+        Inventario Pablo = new Inventario();
+             Pablo.addItem("tomates", 3);
+             Pablo.addItem("catsup", 2);
+             Pablo.addItem("2",2);
+             Pablo.addItem("2",2);
+             Pablo.addItem("2",2);
+           //  System.out.println(Pablo.numeroDeArticulos());
+           //  System.out.println(Pablo.numeroDeArticulosEspecificos("tomates"));
+////////////////////////////////////        ArrayList<String> Inventario = new ArrayList<String>();
+////////////////////////////////////        Inventario.add("Cosa1");
+////////////////////////////////////        Inventario.add("Cosa2");
+////////////////////////////////////        Inventario.add("Cosa3");
+////////////////////////////////////        Inventario.add("Cosa4");
+////////////////////////////////////        Inventario.add("Cosa5");
+////////////////////////////////////        Inventario.add("Cosa6");
+////////////////////////////////////        Inventario.remove(Inventario.indexOf("Cosa2"));
+////////////////////////////////////        for(int i=0; i<Inventario.size(); i++)
+////////////////////////////////////        System.out.println(Inventario.get(i));
+////////////////////////////////////        Inventario.add(ListaNueva(Class<T>klazz));        
     }
-    public <T> List<T> ListaNueva(Class<T>klazz){
-   List<T> lista = new ArrayList<>();
-   return lista;
-   }
+     
+    
+    
+    
+    
+    
     
 //    public void agregaUnaLista(){
 //    Inventario.add(ListaNueva()); //checar el error en esta linea
